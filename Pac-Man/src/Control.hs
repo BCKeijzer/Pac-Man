0fd32c7c369bf241283e8cf4ed2294d70181aaf8
BCKeijzer <38952290+BCKeijzer@users.noreply.github.com>
2018-10-16 12:37:09 +0200

    Delete Control.hs

--- Pac-Man/src/Control.hs
+++
@@ -1,8 +0,0 @@
-module Control where
-
-import DTypes
-import Graphics.Gloss
-
--- | In deze functie wordt de gamestate aangepast wanneer er een key wordt gebruikt
-handleKeys :: Event -> GameState -> GameState
-handleKeys = undefined
